932373900ab35c4e42c7954c9c4ac4cb5ce0c28c
alexandre santos grisende <alexandrehgrisende@hotmail.com>
2022-02-18 21:12:59 -0300

    update: Models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public $table = 'products';
+
+    public $timestamps = false;
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'sku',
+        'insertion_date'
+    ];
+
 }
--- app/Models/Withdraw.php
+++ app/Models/Withdraw.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Withdraw extends Model
 {
     use HasFactory;
+
+    public $table = 'withdraws';
+
+    public $timestamps = false;
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'sku',
+        'insertion_date'
+    ];
 }
