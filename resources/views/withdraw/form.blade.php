bf355964469081cd622fa5555f1dd9da93d3b551
alexandre santos grisende <alexandrehgrisende@hotmail.com>
2022-02-21 20:06:40 -0300

    git add app/Domain/Models/Product.php

---
+++ resources/views/withdraw/form.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.layout')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+    <div class="form">
+        <div class="mt-5 p-5 w-50 mx-auto border">
+            @if(isset($withdraw))
+                <form class="inline-form p-5" method="post" action="{{url('withdraw', $withdraw['id'])}}">
+                @method('PUT')
+            @else
+                <form class="inline-form p-5" method="post" action="{{url('withdraw/')}}">
+            @endif
+                @csrf
+                    <div class="mt-3">
+                        <label for="title">Product</label>
+                        <select class="form-control" id="product_id" name="product_id" required>
+                            @foreach($withdraws as $product)
+                                <option value="{{with['product_id']}}">{{$product['name']}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="row g-2 align-items-center mt-3">
+                        <div class="col-8">
+                            <label for="title">SKU</label>
+                            <input type="text" class="form-control" id="sku" name="sku" value="{{$withdraw['sku'] ?? ''}}" required>
+                        </div>
+
+                        <div class="col-4">
+                            <label for="title">Data de Adição</label>
+                            <input type="date" class="form-control" id="withdraw_date" name="withdraw_date" value="{{$withdraw['insertion_date'] ?? ''}}" required>
+                        </div>
+                    </div>
+                    <div class="form-group text-center mt-5">
+                    <a href="{{ url('products/') }}" class="btn btn-primary">Cancelar</a>
+                    <button class="btn btn-primary ml-3"> Salvar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('products')->group(function (){
-    Route::get('/', [\App\Http\Controllers\ProductController::class, 'list']);
-    Route::get('/{id}', [\App\Http\Controllers\ProductController::class, 'getById']);
-    Route::post('/{isApi}', [\App\Http\Controllers\ProductController::class, 'create']);
-    Route::put('/{id}', [\App\Http\Controllers\ProductController::class, 'update']);
-    Route::delete('/{id}', [\App\Http\Controllers\ProductController::class, 'delete']);
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
